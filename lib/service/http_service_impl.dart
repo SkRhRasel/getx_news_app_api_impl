9a560a1fc1d8c053fb67c9f1a12abd71ed14e503
Sk Rockybul Hasan Rasel <sk.rasel.cse@gmail.com>
2021-09-21 18:58:25 +0600

    first commit2

--- lib/service/http_service_impl.dart
+++ lib/service/http_service_impl.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:dio/src/response.dart';
 import 'package:getx_news_app_api_impl/service/http_service.dart';
 
@@ -5,14 +6,21 @@ const BASE_URL = "https://newsapi.org/";
 const API_KEY = "e1dcc535a8bd4a32b03f23490f760c0a";
 
 class HttpServiceImpl implements HttpService {
+  late Dio _dio;
+
   @override
-  Future<Response> getRequest(String url) {
+  Future<Response> getRequest(String url) async {
     // TODO: implement getRequest
-    throw UnimplementedError();
+    try {
+      final response = await _dio.get(url);
+    } on Exception catch (e) {
+      // TODO
+    }
   }
 
   @override
   void init() {
-    // TODO: implement init
+    _dio = Dio(BaseOptions(
+        baseUrl: BASE_URL, headers: {"Authorization": "Bearer $API_KEY"}));
   }
 }
